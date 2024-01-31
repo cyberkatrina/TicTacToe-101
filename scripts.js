9c7548b50b3ef63d4692eca0f89cbcdad32324bf
cyberkatrina <katrinaaliashkevich@utexas.edu>
2024-01-30 20:48:55 -0600

    extra spaces removed

--- scripts.js
+++ scripts.js
@@ -13,10 +13,8 @@ let currentMarker = 'X'
 // this "handleClick" function is called when a box is clicked. Here, "element" will hold the same value as "this" does in the HTML. 
 // "this" is a special word in JS but "element" could have been "thing" or "el" or whatever we wanted it to be as long as we use it again in the "console.log" statement
 const handleClick = (element) => {
-
   // this uses the "log" method on the "console" to log out the element's id so we can see it with our human eyes
   console.log(`The element you clicked on has an id:  ${element.id}`)
-
   // this next line prevents an X being changed to an O or an O being changed to an X by...
   //  checking to see if the square clicked has anything in it, if not continue
   if(!document.getElementById(element.id).innerHTML){
@@ -24,17 +22,13 @@ const handleClick = (element) => {
   }
 }
 
-
 // this function places the "currentMarker" inside the HTML element that was clicked and calls the "changeMarker" function.
 const addMarker = (id) => {
-
   console.log(`*** The current marker is:  ${currentMarker}. ***`)
   console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
-
   document.getElementById(id).innerHTML = currentMarker
   changeMarker()
 }
-
 
 // This "changeMarker" function changes "X" to "O" in the "currentMarker" variable or "O" to "X"
 const changeMarker = () => {
@@ -45,18 +39,13 @@ const changeMarker = () => {
   }
 }
 
-
 // This "resetBoard" function is called when the user clicks on the "Restart" button.
 const resetBoard = () => {
-  
-
   const squares = document.getElementsByTagName("td");
   for (let i = 0; i < squares.length; i++) {
     // will log out the id of each square as it loops over them.
     console.log(squares[i].id)
-
     // sets the innerHTML to null to replace the "X" or "O"
     squares[i].innerHTML = null
   }
-
 }
