a625be05873fa5ec1e2a8cb8fc6fc3f5af515db4
cyberkatrina <katrinaaliashkevich@utexas.edu>
2024-02-17 18:15:28 -0600

    validation logic implemented

--- scripts.js
+++ scripts.js
@@ -1,20 +1,42 @@
-let board = 
-[["", "", ""],
- ["", "", ""], 
- ["", "", ""]]
- 
 // The variable will change from X to O based on what player turn it is. We need to hold this so we can place an X or O on the board when they're clicked.
 let currentMarker = 'X'
 
-winConditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [6, 7, 8], [0, 4, 8], [2, 4, 6]]
+winConditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 squares = document.getElementsByTagName("td")
+let countX
+let countO
 
+const checkForWin = () => {
+  squares = document.getElementsByTagName("td")
+  for (i=0; i < winConditions.length; i++) {
+    countX = 0
+    countO = 0
+    for (j=0; j < 3; j++) {
+      let currentSquare = winConditions[i][j]
+      if (squares[currentSquare].innerHTML == "X") {
+        countX++
+      }
+      else if (squares[currentSquare].innerHTML == "O") {
+        countO++
+      }
+    }
+    if (countX === 3) {
+      setTimeout(() => {
+        window.alert(`Player X won!`)
+      }, 500);
+    }
+    if (countO === 3) {
+      setTimeout(() => {
+        window.alert(`Player O won!`)
+      }, 500);
+    }
+  }
+}
 
 // this "handleClick" function is called when a box is clicked. Here, "element" will hold the same value as "this" does in the HTML. 
 // "this" is a special word in JS but "element" could have been "thing" or "el" or whatever we wanted it to be as long as we use it again in the "console.log" statement
 const handleClick = (element) => {
   // this uses the "log" method on the "console" to log out the element's id so we can see it with our human eyes
-  console.log(`The element you clicked on has an id:  ${element.id}`)
   // this next line prevents an X being changed to an O or an O being changed to an X by...
   //  checking to see if the square clicked has anything in it, if not continue
   if(!document.getElementById(element.id).innerHTML){
@@ -24,13 +46,9 @@ const handleClick = (element) => {
 
 // this function places the "currentMarker" inside the HTML element that was clicked and calls the "changeMarker" function.
 const addMarker = (id) => {
-  console.log(`*** The current marker is:  ${currentMarker}. ***`)
-  console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
   document.getElementById(id).innerHTML = currentMarker
-  const row = parseInt(id.charAt(0))
-  const column = parseInt(id.charAt(2))
-  board[row][column] = currentMarker
   checkForWin()
+  changeMarker()
 }
 
 // This "changeMarker" function changes "X" to "O" in the "currentMarker" variable or "O" to "X"
@@ -44,56 +62,10 @@ const changeMarker = () => {
 
 // This "resetBoard" function is called when the user clicks on the "Restart" button.
 const resetBoard = () => {
-  const squares = document.getElementsByTagName("td");
   for (let i = 0; i < squares.length; i++) {
     // will log out the id of each square as it loops over them.
-    console.log(squares[i].id)
     // sets the innerHTML to null to replace the "X" or "O"
     squares[i].innerHTML = null
   }
-  board = 
-  [["", "", ""],
-  ["", "", ""], 
-  ["", "", ""]]
-}
-
-const checkForWin = () => {
-if(horizontalWin() || verticalWin() || diagonalWin()) {
-  setTimeout(() => {
-    window.alert(`Player ${currentMarker} won!`)
-  }, 500);
-} else {
-  changeMarker()
-}
-}
-
-
-
-const horizontalWin = () => {
-// Your code here to check for horizontal wins
-  if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") 
-  || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O")
-  || (board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X")
-  || (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O")
-  || (board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X")
-  || (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O")
-  ) {
-    return true
-  }
-}
-
-const verticalWin = () => {
-// Your code here to check for vertical wins
-if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") 
-|| (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")
-|| (board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X")
-|| (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O")
-|| (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O")
-) {
-  return true
-}
-}
-
-const diagonalWin = () => {
-// Your code here to check for diagonal wins
+  currentMarker = "X"
 }
